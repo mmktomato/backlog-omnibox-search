3a40c01c9d1afe13b228eed3929af4045eb1c252
mmktomato <mkt.motohashi@gmail.com>
2020-05-14 22:49:14 +0900

    omit 'authorize' button from options page

--- src/options/optionsView.ts
+++ src/options/optionsView.ts
@@ -1,11 +1,10 @@
 import { debounce } from 'ts-debounce';
-import { LitElement, html, customElement, css } from "lit-element";
+import { LitElement, html, customElement } from "lit-element";
 
 import "./list";
 import type { OnInputHandler } from "./list";
 import type { Options } from "../type";
-import { setTokens, getOptions, setOptions } from "../storage";
-import { authorize } from "../backlog/auth";
+import { getOptions, setOptions } from "../storage";
 
 const setOptionsDebounced = debounce(setOptions, 200);
 
@@ -20,36 +19,14 @@ class OptionsView extends LitElement {
     this.requestUpdate();
   }
 
-  static get styles() {
-    return css`
-      .buttonOuter {
-        text-align: right;
-      }
-
-      button {
-        margin: 0.5em 2em;
-      }
-    `;
-  }
-
   private handleOnInput(...[key, value]: Parameters<OnInputHandler>) {
     this.options[key] = value.trim();
     setOptionsDebounced(this.options);
   }
 
-  private async authorize() {
-    if (this.options.defaultBaseUrl) {
-      const newTokens = await authorize(this.options.defaultBaseUrl);
-      await setTokens(this.options.defaultBaseUrl, newTokens);
-    }
-  }
-
   render() {
     return html`
       <options-list .options=${this.options} .onInput=${this.handleOnInput}></options-list>
-      <div class="buttonOuter">
-        <button @click=${this.authorize}>Authorize</button>
-      </div>
     `;
   }
 }
